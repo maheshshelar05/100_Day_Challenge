407c4b51816d071743ea9c1de2b1a14cd720d983
Mahesh Shelar <mshelar334@gmail.com>
2025-11-28 13:34:48 +0530

    Accept number from console and print it reverse

---
+++ reverseNumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class reverse 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number : ");
+        int num=sc.nextInt();
+
+        int rev=0;
+        while(num!=0)
+        {
+            int rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num / 10;
+        }
+        System.out.println("Reversed Number is : " + rev);
+    }
+}
