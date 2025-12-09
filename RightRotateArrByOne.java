fe3538c93a6638d3fb0981f6c145f61e83336a37
Mahesh Shelar <mshelar334@gmail.com>
2025-12-09 11:35:03 +0530

    Implement one-step right rotation logic for array .

---
+++ RightRotateArrByOne.java
@@ -0,0 +1,21 @@
+class RightRotateArrByOne
+{
+    public static void main(String args[])
+    {
+        int arr[]={5,6,3,4,1};
+
+        int temp=arr[4];
+        arr[4]=arr[3];
+        arr[3]=arr[2];
+        arr[2]=arr[1];
+        arr[1]=arr[0];
+        arr[0]=temp;
+        
+
+         for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
