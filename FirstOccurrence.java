e1138da17cd9aab9dc2e945e681f65383cbcf48b
Mahesh Shelar <mshelar334@gmail.com>
2025-12-25 17:23:44 +0530

    Add program to find first occurrence of substring .

---
+++ FirstOccurrence.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class FirstOccurrenceV2
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter main string:");
+        String str = sc.nextLine();
+
+        System.out.println("Enter substring:");
+        String sub = sc.nextLine();
+
+        int i = 0;
+
+        while(i < str.length())
+        {
+            int k = i;
+            int j = 0;
+
+            while(j < sub.length() && k < str.length())
+            {
+                if(str.charAt(k) != sub.charAt(j))
+                {
+                    break;
+                }
+                k++;
+                j++;
+            }
+
+            if(j == sub.length())
+            {
+                System.out.println("First occurrence at index: " + i);
+                return;
+            }
+            i++;
+        }
+
+        System.out.println("Substring not found");
+    }
+}
