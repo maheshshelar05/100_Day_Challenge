561e558bf198def7a6d523c71926e13c122c5207
Mahesh Shelar <mshelar334@gmail.com>
2026-01-01 22:27:50 +0530

    Find duplicate characters in string using loops .

---
+++ FindDupliChar.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class FindDupliChar
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String :");
+        String str = sc.nextLine();
+
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int found = 0;
+
+            for (int j = i+1; j < str.length(); j++) {
+                if (ch == str.charAt(j)) {
+                    found = 1;
+                    break;
+                }
+
+            }
+                if(found==1)
+                System.out.println("Duplicate Char : "+ ch);
+        }
+    }
+}
